c2d70d86a978a500b4cc046a1b3b4ccba13a5aca
Samiksha Mathur <samiksha@Samikshas-MacBook-Air.local>
2024-01-08 04:56:14 +0530

    [feat] - Add dialog box in single audio video chat room screen

--- lib/Screens/SingleAudioVideoRoomScreen/single_audio_video_room_screen.dart
+++ lib/Screens/SingleAudioVideoRoomScreen/single_audio_video_room_screen.dart
@@ -1,7 +1,11 @@
+import 'package:adaptive_action_sheet/adaptive_action_sheet.dart';
 import 'package:barber_klipz_ui/Resources/color_const.dart';
+import 'package:barber_klipz_ui/Screens/ChatRoomMessageScreen/chat_room_message_screen.dart';
 import 'package:barber_klipz_ui/Screens/SingleAudioVideoRoomScreen/Backend/Provider/single_audio_video_room_base_model.dart';
+import 'package:barber_klipz_ui/Utils/navigator_util.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:badges/badges.dart' as badges;
 
 import '../../Utils/net_image.dart';
 import '../../Utils/text_util.dart';
@@ -44,16 +48,24 @@ class SingleAudioVideoRoomScreen extends ConsumerWidget {
               ),
             ),
             const Spacer(),
-            Container(
-              height: screenUtil.setHeight(30),
-              width: screenUtil.setHeight(30),
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: kGrey,
-              ),
-              child: const Icon(
-                Icons.chat_outlined,
-                size: 20,
+            GestureDetector(
+              onTap: () {
+                NavigatorUtil.push(
+                  context,
+                  screen: const ChatRoomMessageScreen(),
+                );
+              },
+              child: Container(
+                height: screenUtil.setHeight(30),
+                width: screenUtil.setHeight(30),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kGrey,
+                ),
+                child: const Icon(
+                  Icons.chat_outlined,
+                  size: 20,
+                ),
               ),
             ),
             SizedBox(
@@ -75,16 +87,197 @@ class SingleAudioVideoRoomScreen extends ConsumerWidget {
             SizedBox(
               width: screenUtil.setWidth(8),
             ),
-            Container(
-              height: screenUtil.setHeight(30),
-              width: screenUtil.setHeight(30),
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: kGrey,
-              ),
-              child: const Icon(
-                Icons.add,
-                size: 20,
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                        backgroundColor: kWhite,
+                        surfaceTintColor: Colors.transparent,
+                        insetPadding:
+                            const EdgeInsets.symmetric(horizontal: 52),
+                        contentPadding: const EdgeInsets.all(18),
+                        content: SizedBox(
+                          height: screenUtil.setHeight(310),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextUtil.secondaryText(
+                                text: "Ping someone in the room",
+                                color: kBlack,
+                                size: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              SizedBox(
+                                height: screenUtil.setHeight(8),
+                              ),
+                              Container(
+                                height: screenUtil.setHeight(30),
+                                decoration: BoxDecoration(
+                                  color: kBackgroundDropDown,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: TextFormField(
+                                  cursorColor: kBlack,
+                                  textAlignVertical: TextAlignVertical.center,
+                                  decoration: const InputDecoration(
+                                    contentPadding: EdgeInsets.only(
+                                      top: 13,
+                                      bottom: 13,
+                                      left: 10,
+                                    ),
+                                    hintText: "Search",
+                                    hintStyle: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(20.0),
+                                      ),
+                                      borderSide: BorderSide(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(20.0),
+                                      ),
+                                      borderSide: BorderSide(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    prefixIcon: Icon(Icons.search),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: screenUtil.setHeight(12),
+                              ),
+                              GridView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: 4,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  childAspectRatio: 1,
+                                  crossAxisCount: 3,
+                                  crossAxisSpacing: 1,
+                                  mainAxisSpacing: 30,
+                                  mainAxisExtent: screenUtil.setHeight(60),
+                                ),
+                                itemBuilder: (context, index) {
+                                  return badges.Badge(
+                                    position: badges.BadgePosition.bottomEnd(
+                                      bottom: -1,
+                                      end: 10,
+                                    ),
+                                    showBadge: true,
+                                    badgeStyle: const badges.BadgeStyle(
+                                      badgeColor: kGrey,
+                                    ),
+                                    onTap: () {},
+                                    badgeContent: const Icon(
+                                      Icons.add,
+                                      color: kBlack,
+                                      size: 12,
+                                    ),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      width: screenUtil.setHeight(60),
+                                      height: screenUtil.setHeight(60),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(50),
+                                        child: const NetImage(
+                                          uri:
+                                              "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                              SizedBox(
+                                height: screenUtil.setHeight(18),
+                              ),
+                              const Divider(
+                                color: kGrey,
+                                thickness: 1.5,
+                              ),
+                              SizedBox(
+                                height: screenUtil.setHeight(8),
+                              ),
+                              Row(
+                                children: [
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.share),
+                                      SizedBox(
+                                        height: screenUtil.setHeight(5),
+                                      ),
+                                      TextUtil.secondaryText(
+                                        text: "Share",
+                                        color: kBlack,
+                                        size: 11,
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: screenUtil.setWidth(45),
+                                  ),
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.facebook),
+                                      SizedBox(
+                                        height: screenUtil.setHeight(5),
+                                      ),
+                                      TextUtil.secondaryText(
+                                        text: "Tweet",
+                                        color: kBlack,
+                                        size: 11,
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: screenUtil.setWidth(45),
+                                  ),
+                                  Column(
+                                    children: [
+                                      const Icon(Icons.link),
+                                      SizedBox(
+                                        height: screenUtil.setHeight(5),
+                                      ),
+                                      TextUtil.secondaryText(
+                                        text: "Copy Link",
+                                        color: kBlack,
+                                        size: 11,
+                                        fontWeight: FontWeight.w500,
+                                      )
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ));
+                  },
+                );
+              },
+              child: Container(
+                height: screenUtil.setHeight(30),
+                width: screenUtil.setHeight(30),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kGrey,
+                ),
+                child: const Icon(
+                  Icons.add,
+                  size: 20,
+                ),
               ),
             ),
             SizedBox(
@@ -223,7 +416,66 @@ class SingleAudioVideoRoomScreen extends ConsumerWidget {
                     ),
                   ),
                   const Spacer(),
-                  const Icon(Icons.more_horiz_outlined)
+                  GestureDetector(
+                    onTap: () {
+                      showAdaptiveActionSheet(
+                        context: context,
+                        isDismissible: true,
+                        title: Container(
+                          height: 6,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: kTextSubTitle,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ),
+                        androidBorderRadius: 30,
+                        actions: <BottomSheetAction>[
+                          BottomSheetAction(
+                              title: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.chat_rounded,
+                                    color: kBlack,
+                                    size: 25,
+                                  ),
+                                  SizedBox(
+                                    width: screenUtil.setWidth(7),
+                                  ),
+                                  TextUtil.secondaryText(
+                                    text: "Community Guidelines",
+                                    color: kBlack,
+                                    size: 15,
+                                    fontWeight: FontWeight.w500,
+                                  )
+                                ],
+                              ),
+                              onPressed: (context) {}),
+                          BottomSheetAction(
+                              title: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.report_gmailerrorred,
+                                    color: kError,
+                                    size: 28,
+                                  ),
+                                  SizedBox(
+                                    width: screenUtil.setWidth(7),
+                                  ),
+                                  TextUtil.secondaryText(
+                                    text: "Report room",
+                                    color: kError,
+                                    size: 15,
+                                    fontWeight: FontWeight.w500,
+                                  )
+                                ],
+                              ),
+                              onPressed: (context) {}),
+                        ],
+                      );
+                    },
+                    child: const Icon(Icons.more_horiz_outlined),
+                  )
                 ],
               ),
               SizedBox(
