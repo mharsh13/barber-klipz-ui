05715908a04d2f928e39bb6cf7474eb034eec0a2
Samiksha Mathur <samiksha@Samikshas-MacBook-Air.local>
2024-01-15 03:46:45 +0530

    [fix] - public sharing commented in add caption screen

--- lib/Screens/AddPostScreen/add_caption_screen.dart
+++ lib/Screens/AddPostScreen/add_caption_screen.dart
@@ -103,38 +103,38 @@ class AddCaptionScreen extends ConsumerWidget {
                       SizedBox(
                         height: screenUtil.setHeight(40),
                       ),
-                      Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: screenUtil.setWidth(8)),
-                            padding: const EdgeInsets.all(2),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                border: Border.all(color: kTextSubTitle)),
-                            child: TextUtil.secondaryText(
-                              text: "#Hashtags",
-                              color: kBlack,
-                              size: 12,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                                horizontal: screenUtil.setWidth(8)),
-                            padding: const EdgeInsets.all(2),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                border: Border.all(color: kTextSubTitle)),
-                            child: TextUtil.secondaryText(
-                              text: "@Users",
-                              color: kBlack,
-                              size: 12,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      )
+                      // Row(
+                      //   children: [
+                      //     Container(
+                      //       margin: EdgeInsets.symmetric(
+                      //           horizontal: screenUtil.setWidth(8)),
+                      //       padding: const EdgeInsets.all(2),
+                      //       decoration: BoxDecoration(
+                      //           borderRadius: BorderRadius.circular(5),
+                      //           border: Border.all(color: kTextSubTitle)),
+                      //       child: TextUtil.secondaryText(
+                      //         text: "#Hashtags",
+                      //         color: kBlack,
+                      //         size: 12,
+                      //         fontWeight: FontWeight.w500,
+                      //       ),
+                      //     ),
+                      //     Container(
+                      //       margin: EdgeInsets.symmetric(
+                      //           horizontal: screenUtil.setWidth(8)),
+                      //       padding: const EdgeInsets.all(2),
+                      //       decoration: BoxDecoration(
+                      //           borderRadius: BorderRadius.circular(5),
+                      //           border: Border.all(color: kTextSubTitle)),
+                      //       child: TextUtil.secondaryText(
+                      //         text: "@Users",
+                      //         color: kBlack,
+                      //         size: 12,
+                      //         fontWeight: FontWeight.w500,
+                      //       ),
+                      //     ),
+                      //   ],
+                      // )
                     ],
                   )
                 ],
@@ -198,140 +198,140 @@ class AddCaptionScreen extends ConsumerWidget {
             const Divider(
               color: kTextSubTitle,
             ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: screenUtil.setWidth(5)),
-                  child: const Icon(
-                    Icons.stars,
-                    size: 32,
-                  ),
-                ),
-                SizedBox(
-                  width: screenUtil.setWidth(10),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextUtil.secondaryText(
-                      text: "Exclusive Content:",
-                      color: kBlack,
-                      size: 12,
-                    ),
-                    SizedBox(
-                      height: screenUtil.setHeight(7),
-                    ),
-                    baseModel.exclusiveContent == true
-                        ? Container(
-                            padding: const EdgeInsets.all(3),
-                            color: kGreen,
-                            child: TextUtil.secondaryText(
-                              text: "Sharing to Subscribers only",
-                              color: kBlack,
-                              size: 12,
-                            ),
-                          )
-                        : Container(
-                            padding: const EdgeInsets.all(3),
-                            color: kCategoryText.withOpacity(0.7),
-                            child: TextUtil.secondaryText(
-                              text: "Public Sharing",
-                              color: kBlack,
-                              size: 12,
-                            ),
-                          ),
-                  ],
-                ),
-                const Spacer(),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: screenUtil.setWidth(5),
-                  ),
-                  child: SizedBox(
-                    width: 55,
-                    height: 35,
-                    child: FittedBox(
-                      fit: BoxFit.fill,
-                      child: CupertinoSwitch(
-                        value: baseModel.exclusiveContent,
-                        activeColor: kGreen,
-                        trackColor: kGrey,
-                        thumbColor: kWhite,
-                        onChanged: (value) {
-                          baseModel.enableExclusiveContent(value);
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Divider(
-              color: kTextSubTitle,
-            ),
-            baseModel.exclusiveContent == true
-                ? Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: screenUtil.setWidth(5)),
-                        child: const Icon(
-                          Icons.content_paste_rounded,
-                          size: 30,
-                        ),
-                      ),
-                      SizedBox(
-                        width: screenUtil.setWidth(10),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TextUtil.secondaryText(
-                            text: "Exclusive Content Agreement:",
-                            color: kBlack,
-                            size: 12,
-                          ),
-                          SizedBox(
-                            height: screenUtil.setHeight(5),
-                          ),
-                          TextUtil.secondaryText(
-                            text:
-                                "In order to post premium content you\nmust assure that you own the material.\nAny infringement of content ownership\nwill be flagged.",
-                            color: kBlack,
-                            size: 11,
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          right: screenUtil.setWidth(5),
-                        ),
-                        child: SizedBox(
-                          width: 55,
-                          height: 35,
-                          child: FittedBox(
-                            fit: BoxFit.fill,
-                            child: CupertinoSwitch(
-                              value: baseModel.exclusiveContentAgreement,
-                              activeColor: kGreen,
-                              trackColor: kGrey,
-                              thumbColor: kWhite,
-                              onChanged: (value) {
-                                baseModel
-                                    .enableExclusiveContentAgreement(value);
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                : Container(),
-            baseModel.exclusiveContent == true
-                ? const Divider(
-                    color: kTextSubTitle,
-                  )
-                : Container(),
+            // Row(
+            //   children: [
+            //     Padding(
+            //       padding: EdgeInsets.only(left: screenUtil.setWidth(5)),
+            //       child: const Icon(
+            //         Icons.stars,
+            //         size: 32,
+            //       ),
+            //     ),
+            //     SizedBox(
+            //       width: screenUtil.setWidth(10),
+            //     ),
+            //     Column(
+            //       crossAxisAlignment: CrossAxisAlignment.start,
+            //       children: [
+            //         TextUtil.secondaryText(
+            //           text: "Exclusive Content:",
+            //           color: kBlack,
+            //           size: 12,
+            //         ),
+            //         SizedBox(
+            //           height: screenUtil.setHeight(7),
+            //         ),
+            //         baseModel.exclusiveContent == true
+            //             ? Container(
+            //                 padding: const EdgeInsets.all(3),
+            //                 color: kGreen,
+            //                 child: TextUtil.secondaryText(
+            //                   text: "Sharing to Subscribers only",
+            //                   color: kBlack,
+            //                   size: 12,
+            //                 ),
+            //               )
+            //             : Container(
+            //                 padding: const EdgeInsets.all(3),
+            //                 color: kCategoryText.withOpacity(0.7),
+            //                 child: TextUtil.secondaryText(
+            //                   text: "Public Sharing",
+            //                   color: kBlack,
+            //                   size: 12,
+            //                 ),
+            //               ),
+            //       ],
+            //     ),
+            //     const Spacer(),
+            //     Padding(
+            //       padding: EdgeInsets.only(
+            //         right: screenUtil.setWidth(5),
+            //       ),
+            //       child: SizedBox(
+            //         width: 55,
+            //         height: 35,
+            //         child: FittedBox(
+            //           fit: BoxFit.fill,
+            //           child: CupertinoSwitch(
+            //             value: baseModel.exclusiveContent,
+            //             activeColor: kGreen,
+            //             trackColor: kGrey,
+            //             thumbColor: kWhite,
+            //             onChanged: (value) {
+            //               baseModel.enableExclusiveContent(value);
+            //             },
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // const Divider(
+            //   color: kTextSubTitle,
+            // ),
+            // baseModel.exclusiveContent == true
+            //     ? Row(
+            //         children: [
+            //           Padding(
+            //             padding: EdgeInsets.only(left: screenUtil.setWidth(5)),
+            //             child: const Icon(
+            //               Icons.content_paste_rounded,
+            //               size: 30,
+            //             ),
+            //           ),
+            //           SizedBox(
+            //             width: screenUtil.setWidth(10),
+            //           ),
+            //           Column(
+            //             crossAxisAlignment: CrossAxisAlignment.start,
+            //             children: [
+            //               TextUtil.secondaryText(
+            //                 text: "Exclusive Content Agreement:",
+            //                 color: kBlack,
+            //                 size: 12,
+            //               ),
+            //               SizedBox(
+            //                 height: screenUtil.setHeight(5),
+            //               ),
+            //               TextUtil.secondaryText(
+            //                 text:
+            //                     "In order to post premium content you\nmust assure that you own the material.\nAny infringement of content ownership\nwill be flagged.",
+            //                 color: kBlack,
+            //                 size: 11,
+            //               ),
+            //             ],
+            //           ),
+            //           const Spacer(),
+            //           Padding(
+            //             padding: EdgeInsets.only(
+            //               right: screenUtil.setWidth(5),
+            //             ),
+            //             child: SizedBox(
+            //               width: 55,
+            //               height: 35,
+            //               child: FittedBox(
+            //                 fit: BoxFit.fill,
+            //                 child: CupertinoSwitch(
+            //                   value: baseModel.exclusiveContentAgreement,
+            //                   activeColor: kGreen,
+            //                   trackColor: kGrey,
+            //                   thumbColor: kWhite,
+            //                   onChanged: (value) {
+            //                     baseModel
+            //                         .enableExclusiveContentAgreement(value);
+            //                   },
+            //                 ),
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       )
+            //     : Container(),
+            // baseModel.exclusiveContent == true
+            //     ? const Divider(
+            //         color: kTextSubTitle,
+            //       )
+            //     : Container(),
           ],
         ),
       ),
