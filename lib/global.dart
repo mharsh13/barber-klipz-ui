2f53c547ebd0b15bb3a9701c4f739ea2f81b7196
Samiksha Mathur <samiksha@Samikshas-MacBook-Air.local>
2024-02-18 16:39:18 +0530

    [fix] - global file

--- lib/global.dart
+++ lib/global.dart
@@ -1,10 +1,6 @@
 class Global {
   // static String baseURL = "https://barberklipz.vercel.app/";
   static String baseURL =
-<<<<<<< HEAD
       "https://barber-klipz-backend-unajfxubgq-el.a.run.app/";
-=======
-      "https://3e61-2405-201-1f-1831-a9f2-9b29-95f9-35aa.ngrok-free.app/";
->>>>>>> 3006320dd907274631b675c10e67e0be4d8351c0
   static String jwt = "";
 }
